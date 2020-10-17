389274b5259cf840a8a261e1208b1737c3f58025
needle1989 <edwardskirk@163.com>
2020-10-17 21:36:55 +0800

    minor changes

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,9 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Raven
+ */
 @SpringBootApplication
 public class DemoApplication {
 

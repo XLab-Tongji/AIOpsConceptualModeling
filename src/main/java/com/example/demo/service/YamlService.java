802f5f611c1b45a083e46887ceec61113d81e813
mmt200088 <13053572652@163.com>
2020-12-15 18:15:04 +0800

    new

--- src/main/java/com/example/demo/service/YamlService.java
+++ src/main/java/com/example/demo/service/YamlService.java
@@ -10,25 +10,12 @@ import com.example.demo.model.*;
  * @author xjt
  */
 public class YamlService {
-<<<<<<< HEAD
+
     public String layer;
     public YamlService(String Layer) {
         this.layer = Layer;
     }
-
-=======
-    private String layer;
-    YamlDao myyaml;
-    public YamlService(String layer) {
-        this.layer = layer;
-        if(layer.equals("API")){myyaml=new YamlDao("API 11.17.yml");}
-        else if(layer.equals("SDK")){myyaml=new YamlDao("SDK 12.8.yml");}
-        else{myyaml=new YamlDao("view model 12.15.yml");}
-    }
-    OntService ontService = new OntService(this.layer);
-
     AbstractModel primeModel = new AbstractModel();
->>>>>>> 8be25662393388a102dfd3a80e3c5dd89830f002
 
     public AbstractModel YamlToProps() throws FileNotFoundException {
 
